df5480205181c5b64e53d152438651ecd84c6423
Thilliê Cristina Moreira de Souza <thillie.moreira@gmail.com>
2024-05-20 13:51:48 -0300

    Criação de projeto Spring com 3 endpoints

--- helloworld/src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -10,12 +10,22 @@ public class HelloWorldController {
 	
 	@GetMapping
 	public String ola() {
-		return "Olá mundo! Bom dia!";
+		return "Olá mundo!";
 	}
 	
 	@GetMapping("comidas")
 	public String comidasFavoritas() {
 		return "sushi \nhamburguer";
+	}
+	
+	@GetMapping("bsms")
+	public String competenciasComportmentais() {
+		return "Comunicação, \nProatividade, \nOrientação ao detalhe, \nTrabalho em equipe";
+	}
+	
+	@GetMapping("objetivosdeaprendizagem")
+	public String objetivosDaSemana() {
+		return "Exercer os seguintes objetivos: \nRever os conteúdos estudados durante a semana,\nTirar dúvidas com mais frequência, \nRealizar exercícios para a melhorar fixação dos conteúdos.";
 	}
 
 }
